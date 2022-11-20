6ebbc1a8b364312a2721c270f9be400ae681b0c3
amoralesqtyo <ale3191@gmail.com>
2022-11-20 16:21:48 +0100

    Laravel Sail init and database structure creation
    - Created reduced container with mysql database
    - Added Company and Employee model, migration, factory and seeders

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create random companies
+        Company::factory(5)->create();
+    }
+}
---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Employee;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create and add random employees to the created companies
+        Employee::factory(30)->create();
+    }
+}
